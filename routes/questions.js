e54ce1897e12d2d98800922d358875ff02d0a860
Russell Johnson <blueyorange@gmail.com>
2022-11-12 09:30:57 +0000

    removed ref to currentUser

--- routes/questions.js
+++ routes/questions.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Question = require("../models/question.model.js");
 
 router.get("/", (req, res) => {
-    return res.render("questions.njk", { currentUser: req.user });
+    return res.render("questions.njk");
 });
 
 module.exports = router;
