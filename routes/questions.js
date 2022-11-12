65a04d0a0605e25bd05ce9b415f0ba8774ac6306
Russell Johnson <blueyorange@gmail.com>
2022-11-12 10:45:32 +0000

    submit question form

--- routes/questions.js
+++ routes/questions.js
@@ -10,4 +10,10 @@ router.get("/create", (req, res) => {
     return res.render("edit-question.njk")
 })
 
+router.post("/create", (req, res) => {
+    const form = req.body;
+    console.log(form)
+    return res.render("edit-question.njk")
+})
+
 module.exports = router;
