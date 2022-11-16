cf08354a4098c7671a3afad851935c7a7bc0ac86
Russell <blueyorange@gmail.com>
2022-11-16 15:45:42 +0000

    seed function

--- models/question.model.js
+++ models/question.model.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 const QuestionSchema = new mongoose.Schema({
-    body: {type: String},
-    options: { type: Map, of: String },
-    correct: {type: String},
-    tags: [{ type: String }],
+  body: { type: String },
+  options: [{ type: String }],
+  correct: [{ type: String }],
+  tags: [{ type: String }],
+  source: { type: String },
+  images: [{ type: String }],
+  type: { type: String },
 });
 
 module.exports = mongoose.model("Question", QuestionSchema);
--- seed/seed.js
+++ seed/seed.js
@@ -1,1 +1,19 @@
-const path = require('path')
+const fs = require("fs");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const Question = require("../models/question.model");
+
+async function run() {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  let rawdata = fs.readFileSync("./seed/questions.json");
+  let questions = JSON.parse(rawdata).questions;
+  console.log(questions);
+
+  await Question.insertMany(questions).then((res) => console.log(res));
+  console.log("Finished.");
+}
+
+run();
