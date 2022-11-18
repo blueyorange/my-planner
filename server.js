8a51a61837afd08d828fde1fba9c82a61ef09ffa
Russell Johnson <blueyorange@gmail.com>
2022-11-18 05:59:15 +0000

    added ignoreUndefined to mongoose options

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ nunjucks.configure("views", {
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  ignoreUndefined: true
 });
 
 //Get the default connection
