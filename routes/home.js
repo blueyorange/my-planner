b3fd8827dad7729d279768e22ae44b2cc0066af3
Russell <blueyorange@gmail.com>
2022-11-23 12:11:23 +0000

    error handling more on polling

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  return res.render("home.njk", { currentUser: req.user });
+  return res.render("home.njk");
 });
+
+router.get("/:joinCode");
 
 module.exports = router;
