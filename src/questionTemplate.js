e8774bddc711109724f385053c4491d28dd7e45d
Russell Johnson <blueyorange@gmail.com>
2022-12-10 15:13:58 +0000

    c

--- src/questionTemplate.js
+++ src/questionTemplate.js
@@ -13,41 +13,25 @@ export default function questionTemplateFunc(question, { disabled }) {
   const bodyHTML = marked.parse(body);
   const choicesHTML = choices.map((c) => marked.parse(c));
   return html`
-    <style>
-      .question {
-        max-width: 100%;
-      }
-      .question img {
-        width: 100%;
-        height: auto;
-      }
-      .question input[type="radio"] {
-        min-width: 5em;
-        appearance: none;
-        margin: 0;
-      }
-      .question label {
-      }
-      .question label:hover {
-      }
-    </style>
-    ${bodyHTML}
-    ${choicesHTML
-      .map(
-        (choice, i) => html`
-          <div class="question">
-            <label class="form-check-label" for="choice-${i}">
-              <input
-                name="answer"
-                type="radio"
-                name="answer"
-                id="choice-${i}"
-              />
-              ${choice}
-            </label>
-          </div>
-        `
-      )
-      .join("")}
+    <div class="question">
+      ${bodyHTML}
+      <form>
+        ${choicesHTML
+          .map(
+            (choice, i) => html`
+              <label class="form-check-label" for="choice-${i}">
+                <input
+                  name="answer"
+                  type="radio"
+                  name="answer"
+                  id="choice-${i}"
+                />
+                ${choice}
+              </label>
+            `
+          )
+          .join("")}
+      </form>
+    </div>
   `;
 }
